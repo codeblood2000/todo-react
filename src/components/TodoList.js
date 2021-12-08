6de03632e9575302a47900a55cae23a857b16717
yash jain <codeblood2000@gmail.com>
2021-12-09 00:36:46 +0530

    updated to new package

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,16 @@
-import Todo from './Todo';
+import React from "react";
+import Todo from "./Todo";
 
-const TodoList = ({ todos, setTodos, status}) => {
-    return (
-        <div className="todo-container">
-            <ul className="todo-list">
-                {todos.map(todo =>
-                    (<Todo key={todo.id} todo={todo} setTodos={setTodos} todos={todos} />)
-                )}
-            </ul>
-
-        </div>
-    );
-}
+const TodoList = ({ todos, setTodos, status }) => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} setTodos={setTodos} todos={todos} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default TodoList;
